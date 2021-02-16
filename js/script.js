695a97e2ebd60f518fd8a81c8db00c976a85a60f
gigagogashvili2003 <75528697+gigagogashvili2003@users.noreply.github.com>
2021-02-16 07:54:23 -0800

    added somethings

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+const secretNumber = Math.trunc(Math.random() * 100 + 1);
+
+let score = 10;
+
+
+
+document.querySelector('.currentScore').textContent = score;
+
+document.querySelector('.question-sign').textContent = secretNumber;
+
+document.querySelector('.check-number').addEventListener('click', function () {
+    
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess, guess);
+    if(!guess){
+
+        document.querySelector('.lets-start').textContent = 'Please write a number!😏'
+        document.querySelector('.lets-start').style.color = 'lightblue';
+
+    }else if(guess === secretNumber){
+        document.querySelector('.lets-start').textContent = 'OMG 😱 a number is correct!✅'
+        document.querySelector('.lets-start').style.color = 'lightgreen';
+        document.querySelector('.currentHighScore').textContent = score;
+
+    }else if (guess > secretNumber) {
+        if(score > 1){
+            document.querySelector('.lets-start').textContent = 'Oops too High😑'
+            document.querySelector('.lets-start').style.color = 'red';
+            score--;
+            document.querySelector('.currentScore').textContent = score;
+        }else {
+            document.querySelector('.lets-start').textContent = 'Oops You lost the game!😑'
+            document.querySelector('.lets-start').style.color = 'yellow';
+            document.querySelector('.currentScore').textContent = 0;
+        }
+    }else if (guess < secretNumber) {
+        if(score > 1){
+            document.querySelector('.lets-start').textContent = 'Oops too High😑'
+            document.querySelector('.lets-start').style.color = 'red';
+            score--;
+            document.querySelector('.currentScore').textContent = score;
+        }else {
+            document.querySelector('.lets-start').textContent = 'Oops You lost the game!😑'
+            document.querySelector('.lets-start').style.color = 'yellow';
+            document.querySelector('.currentScore').textContent = 0;
+        }
+    }
+
+})
